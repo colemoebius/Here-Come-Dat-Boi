a7f2082383d6907103a075df0135b8e35813305a
colemoebius <colemoebius1@gmail.com>
2016-05-05 08:11:56 -0700

    Here come dat boi (test level)
    oh shit waddup

---
+++ HSCStealthGame/Assets/Scripts/Controls.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+
+	public float speed = 1f;
+	public float jumpSpeed = 240f;
+	public float forwardSpeed = 20;
+	private Rigidbody2D body2d;
+	private Animator animator;
+	private AudioSource source;
+	public AudioClip punch; 
+	public float vol  = 0.25f;
+
+	void Awake(){
+		body2d = GetComponent<Rigidbody2D> ();
+		source = GetComponent<AudioSource> ();
+	}
+	void Start () {		
+		animator = GetComponent<Animator>();
+	}
+
+
+	void Update() {
+
+
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += new Vector3(speed*Time.deltaTime, 0.0f, 0.0f);
+			animator.SetBool ("Running", true);
+			animator.SetBool("Idle", false);
+			animator.SetBool("Punching",false);
+			
+		}
+		
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position -= new Vector3(speed*Time.deltaTime, 0.0f, 0.0f);
+			animator.SetBool ("Running", true);
+			animator.SetBool("Idle", false);
+			animator.SetBool("Punching",false);
+
+
+
+					}
+		
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			GetComponent<Rigidbody2D>().AddForce(-Vector2.up * speed);
+		}
+		
+		if(Input.GetKey(KeyCode.UpArrow)) {
+
+			animator.SetBool ("Running", true);
+			animator.SetBool("Idle", false);
+			animator.SetBool("Punching",false);
+		}
+		if (Input.GetKey (KeyCode.Z)) {
+			animator.SetBool("Punching",true);
+			animator.SetBool("Idle", false);
+			animator.SetBool ("Running", false);
+			source.PlayOneShot(punch,vol); 
+		}
+		if (Input.anyKey == false) {
+			animator.SetBool("Idle",true);
+			animator.SetBool("Running", false);
+			animator.SetBool("Punching",false);
+		}
+	}
+
+}
+
+	// Use this for initialization
+
+	// Update is called once per frame
+
---
+++ HSCStealthGame/Assets/Scripts/FlipperControls.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperControls : MonoBehaviour {
+	private Animator animator;
+	private Rigidbody2D myRigidbody;
+	private float movementSpeed;
+	private bool facingRight;
+	private bool attacking;
+
+	// Use this for initialization
+	void Start () {
+		facingRight = true;
+		myRigidbody = GetComponent<Rigidbody2D> ();
+
+	}
+
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		HandleMovement (horizontal);
+		//HandleAttacks();
+		Flip (horizontal);
+	}
+	void Update(){
+		//HandleInput ();
+	}
+
+	private void HandleMovement(float horizontal){
+
+		myRigidbody.velocity = new Vector2(horizontal * movementSpeed,myRigidbody.velocity.y);
+
+	}
+	private void HandleAttacks(){
+		if (attacking) {
+			animator.SetTrigger("Punching");
+		}
+	}
+	private void HandleInput(){
+		if (Input.GetKey (KeyCode.Z)) {
+			attacking=true;
+		}
+	}
+	private void Flip(float horizontal){
+		if (horizontal > 0 && !facingRight || horizontal<0 && facingRight) {
+			facingRight = !facingRight;
+
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+}
