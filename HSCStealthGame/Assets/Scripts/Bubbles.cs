d9c95aedb443862420a4b4609e9f3e75715b2eec
colemoebius <colemoebius1@gmail.com>
2016-05-12 09:25:14 -0700

    heat pipe done, level 1 done
    :100: :100: :100:

---
+++ HSCStealthGame/Assets/Scripts/Bubbles.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bubbles : MonoBehaviour {
+	
+	public float routeLenght = 10;
+	public float verticalSpeed = 10;
+	[Range (0,1)]
+	public float startRelativePosition=0;
+	
+	public bool goUp;
+	Vector3 basePosition, remotePosition;
+	void Start(){
+		basePosition = transform.position;
+		remotePosition = getRemotePosition();
+		transform.position = Vector3.Lerp(basePosition, remotePosition, startRelativePosition);
+		updateVelocity();
+	}
+	
+	Vector2 velocity;
+	void Update(){
+
+			goUp = true;
+			updateVelocity();
+
+		GetComponent<Rigidbody2D>().velocity = velocity;
+	}
+	
+	void updateVelocity(){
+		velocity = new Vector2(0, goUp ?  verticalSpeed : -verticalSpeed);
+	}
+	
+	Vector3 getRemotePosition(){
+		return basePosition + new Vector3(0, routeLenght, 0);
+	}
+	
+	void OnDrawGizmos(){
+		if (Application.isPlaying){
+			Gizmos.DrawLine(basePosition, remotePosition);
+		} else {			
+			Vector2 boxSize = (GetComponent<Collider2D>() as BoxCollider2D).size; 
+			basePosition = transform.position;
+			remotePosition = getRemotePosition();
+			Gizmos.DrawLine(transform.position, remotePosition);
+			Gizmos.DrawCube(Vector3.Lerp(basePosition, remotePosition, startRelativePosition),boxSize);
+		}
+		
+	}
+
+}
---
+++ HSCStealthGame/Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+	public float delay = 2.0f;
+	public bool active = true;
+	public Vector2 delayRange = new Vector2(1, 2);
+	private AudioSource source;
+	public AudioClip zombie; 
+	public float vol  = 0.25f;
+
+	void Awake(){
+
+		source = GetComponent<AudioSource> ();
+	}
+	// Use this for initialization
+	void Start () {
+		ResetDelay ();
+		StartCoroutine (EnemyGenerator ());
+		source.PlayOneShot(zombie,vol); 
+	}
+
+	IEnumerator EnemyGenerator(){
+
+		yield return new WaitForSeconds (delay);
+
+		if (active) {
+			var newTransform = transform;
+			source.PlayOneShot(zombie,vol); 
+
+			GameObjectUtil.Instantiate(prefabs[Random.Range(1, prefabs.Length)], newTransform.position);
+			ResetDelay();
+		}
+
+		StartCoroutine (EnemyGenerator ());
+
+	}
+
+	void ResetDelay(){
+		delay = Random.Range (delayRange.x, delayRange.y);
+	}
+
+}
